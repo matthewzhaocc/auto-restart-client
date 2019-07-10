8ba127e5b8a7813e95c3a0e97c735afb6ad4eac1
zhixianMatthewZhao <46915203+zhixianMatthewZhao@users.noreply.github.com>
2019-07-10 00:00:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var wol = require('wol');
 const ping = require('ping');
 console.log('function is running');
 function teststat(){
-  ping.sys.probe('10.20.0.80', function(islive){
-    islive?console.log(''):wol.wake('e0:d5:5e:a9:5e:24')
+  ping.sys.probe('IP_ADDR', function(islive){
+    islive?console.log(''):wol.wake('MAC_ADDR')
     });}
 teststat();
